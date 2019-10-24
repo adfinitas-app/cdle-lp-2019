c805713efefa11ea45fcf71fb8d9f37848de4b03
Maurin Liberal <mliberal@dhcp133.adfinitas.fr>
2019-10-24 17:23:30 +0200

    [+] don.html don.css t-shirt.html t-shirt.css js for t-shirt and assets

---
+++ js/t-shirt.js
@@ -0,0 +1,10 @@
+(function() {
+    $("#click_t-shirt").click(function() {
+        console.log("lol");
+        if ($(".img_t-shirt").attr("src") == "assets/t-shirt-front.png") {
+            $(".img_t-shirt").attr("src", "assets/t-shirt-back.png")
+        } else {
+            $(".img_t-shirt").attr("src", "assets/t-shirt-front.png")
+        }
+    });
+})();
