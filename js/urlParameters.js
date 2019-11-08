c3cff00654ee89993311bf90b13bb2c42f1da004
Maurin Liberal <mliberal@dhcp133.adfinitas.fr>
2019-11-08 10:28:17 +0100

    Add Favicon and delet java for meta

--- js/urlParameters.js
+++
@@ -1,8 +0,0 @@
-var c = url.searchParams.get("enfant");
-
-$('meta[property="og:title"]').remove();
-$('meta[property="og:description"]').remove();
-$('meta[property="og:url"]').remove();
-$("head").append('<meta property="og:title" content="blubb1">');
-$("head").append('<meta property="og:description" content="blubb2">');
-$("head").append('<meta property="og:url" content="blubb3">');
