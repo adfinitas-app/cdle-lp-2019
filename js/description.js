f6e884fc8da57ea6e1936527e855146173b30c91
thibaut trouve <thibaut.trouve@epitech.eu>
2020-10-16 02:18:52 +0200

    fix 1

--- js/description.js
+++ js/description.js
@@ -1,5 +1,6 @@
 function changeColor(e) {
-    e.firstElementChild.style.color = "#f9b000";
+    $(e).find(".kidDesc").css("color", "#f9b000");
+    // e.firstElementChild.style.color = "#f9b000";
 }
 
 function changeBackColor(e) {
