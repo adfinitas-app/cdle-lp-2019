17857a7e0536b149ffe1a54c907e56f1b6043175
thibaut trouve <thibaut.trouve@epitech.eu>
2020-11-20 17:12:41 +0100

    ok

---
+++ js/alain.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	$("#alain-deloche .more").on("click", function () {
+    $("#alain-deloche .more").show();
+    $(this).hide();
+    $("#alain-deloche .title, #alain-deloche .subtitle").toggle("700");
+  });
+
+  $("#alain-deloche .picto-row.ecouter").on("click", function () {
+    $(this).find('.my-hide').show();
+    $(this).find('.img-wrapper').hide();
+    $(this).find('.img-wrapper + span').hide();
+  });
+});
