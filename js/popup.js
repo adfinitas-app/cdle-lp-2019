18a832585966658489d318d744b9d36238c590ce
thibaut trouve <thibaut.trouve@epitech.eu>
2020-11-27 15:10:38 +0100

    popup

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	$(".popup").hide();
+	setTimeout(function () {
+		$(".popup").fadeIn(500);
+		$(".navigation__collapse").addClass("popup-active");
+	}, 30000);
+
+	$("#popup-close").click((event) => {
+		event.preventDefault();
+		$(".popup").fadeOut(100);
+		$(".navigation__collapse").removeClass("popup-active");
+	});
+});
