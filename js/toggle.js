b140877cb74a7322678180e7245949655b048ccb
Max Croquet <80392307+maxcroquet@users.noreply.github.com>
2021-11-15 17:31:41 +0100

    layout update
    Manque tracking

--- js/toggle.js
+++ js/toggle.js
@@ -1,19 +1,29 @@
 function expand(index) {
     let target = document.getElementById(index);
-    document.querySelector(index + ".discover").style.display = "none";
-    target.parentElement.style.height = "720px";
-    target.style.display = "flex";
+    var up = target.parentElement;
+    up.querySelector(".discover").style.display = "none";
+    up.style.height = "1100px";
+    
+
+    setTimeout(function(){ target.style.display = "flex";}, 500);
 }
 
 function reduce(index) {
     let target = document.getElementById(index);
+    var up = target.parentElement;
     target.style.display = "none";
-    target.parentElement.style.height = "320px";
-    document.querySelector( {index} + ".discover").style.display = "flex";
+    up.style.height = "320px";
+    up.querySelector(".discover").style.display = "flex";
 }
 
-function toggle_menu(index) {
-    let target = document.getElementById(index);
-    target.style.display = "none";
-    target.parentElement.style.height = "320px";
+function toggle_menu(index1,index2) {
+    let target = document.getElementById(index1);
+    if(!target.classList.contains("active")){
+        let off = document.getElementById(index2);
+        target.classList.add("active");
+        off.classList.remove("active");
+        document.querySelector("#"+index1+"_content").classList.add("active");
+        document.querySelector("#"+index2+"_content").classList.remove("active");
+    }
 }
+    
